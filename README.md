44b4594533171f51bd1e38fd837b13c9e65571b7
teakzc <93330348+teakzc@users.noreply.github.com>
2026-02-11 08:38:25 +0700

    fixes

--- README.md
+++ README.md
@@ -1,3 +1,68 @@
 # @rbxts/axis
 
-Typings for [axis](https://neond00m.github.io/Axis/)
+roblox-ts typings for [axis](https://neond00m.github.io/Axis/)
+
+## Deviations
+
+### Map<T>
+
+```diff
+- export type Map<T> = { [any]: any }
++ export type _Map<T> = Map<defined, T> | defined[];
+```
+
+### InputConstructor
+
+```diff
+- export type InputConstructor = <T>(Map<T> | (Map<T> & { deadzone: number? })) -> Input<T>
++ export type InputConstructor = <T>(keyMap: _Map<T>, deadzone?: number) => Input<T>;
+```
+### Input Constructor function
+
+```diff
+- local function new<T>(inputMap: types.Map<T> & { deadzone: number? }): types.Input<T>
++ local function new<T>(inputMap: types.Map<T>, deadzone: number?): types.Input<T>
+    local input: types.Input<T> = {
+		vector = false,
+		current = {},
+		previous = {},
+		active = {
+			{}, -- separate input axes for each controller
+		},
+		resets = {}, --resets input on next update
+		connections = {},
+		inputMap = inputMap,
+        - deadzone = inputMap.deadzone
+		+ deadzone = deadzone,
+
+		read = read,
+		pressing = pressing,
+		pressed = pressed,
+		released = released,
+		changed = changed,
+		hold = hold,
+		map = map,
+		update = update,
+		move = move,
+	}
+	map(input, inputMap)
+
+	return input
+end
+```
+
+## Examples
+
+```ts
+input(
+	new Map<Enum.KeyCode, Vector3>([
+		[Enum.KeyCode.Up, new Vector3(0, 1, 0)],
+		[Enum.KeyCode.Down, new Vector3(0, -1, 0)],
+		[Enum.KeyCode.Left, new Vector3(-1, 0, 0)],
+		[Enum.KeyCode.Right, new Vector3(1, 0, 0)],
+	]),
+, 1);
+
+input([Enum.KeyCode.W, Enum.KeyCode.A, Enum.KeyCode.S, Enum.KeyCode.D], 1);
+```
+
--- src/index.d.ts
+++ src/index.d.ts
@@ -1,49 +1,58 @@
-export namespace Axis {
+export type DeviceType = "Desktop" | "Touch" | "Controller";
+
+export type _Map<T> = Map<defined, T> | defined[];
+
+export interface Input<T> {
 	/**
-	 * @within Axis
-	 * @type DeviceType "Desktop" | "Touch" | "Controller"
+	 * Reads current and previous values for the axis
+	 *
+	 * ```ts
+	 * const [current, previous] = attack.read()
+	 * ```
+	 *
+	 * @param controller
 	 */
-	type DeviceType = "Desktop" | "Touch" | "Controller";
+	read(controller?: number): LuaTuple<[T, T]>;
+	pressing(controller?: number): boolean;
+	pressed(controller?: number): boolean;
+	released(controller?: number): boolean;
+	changed(controller?: number): boolean;
+	hold(value: T, controller?: number): void;
+	move(value: T, controller?: number): void;
+	map(keyMap: _Map<T>): void;
+	update(): void;
 
-	/**
-	 * @within Axis
-	 * @type Map<T> { [Enum | string]: T } & { Enum.KeyCode | Enum.UserInputType }
-	 */
-	type _Map<T> = Map<Enum | string, T> & Array<Enum.KeyCode | Enum.UserInputType>;
-
-	interface Input<T> {
-		read: (controller?: number) => LuaTuple<[T, T]>;
-		pressing: (controller?: number) => boolean;
-		pressed: (controller?: number) => boolean;
-		released: (controller?: number) => boolean;
-		changed: (controller?: number) => boolean;
-		hold: (value: T, controller?: number) => void;
-		move: (value: T, controller?: number) => void;
-		map: (keyMap: _Map<T>) => void;
-		update: () => void;
-
-		deadzone?: number;
-		vector: boolean;
-		current: T[];
-		previous: T[];
-		active: Array<Map<defined, T>>;
-		resets: Map<defined, defined>;
-		connections: RBXScriptConnection[];
-		keyMap: _Map<T>;
-		inputMap: Map<Enum.KeyCode | Enum.UserInputType, T>;
-	}
-
-	type InputConstructor = <T>(input: _Map<T> | (_Map<T> & { deadzone?: number })) => Input<T>;
-
-	interface Axis {
-		device: (inputType: Enum.UserInputType | undefined) => DeviceType;
-		update: (inputs: Map<defined, Input<defined>>) => void;
-		input: InputConstructor;
-	}
-
-	function input<T>(inputMap: (_Map<T> & { deadzone?: number }) | _Map<T>): Input<T>;
-
-	function update(inputs: Map<defined, Input<defined>>): void;
-
-	function device(input?: Enum.UserInputType): DeviceType;
+	deadzone?: number;
+	vector: boolean;
+	current: T[];
+	previous: T[];
+	active: Array<Map<defined, T>>;
+	resets: Map<defined, defined>;
+	connections: RBXScriptConnection[];
+	keyMap: _Map<T>;
+	inputMap: Map<Enum.KeyCode | Enum.UserInputType, T>;
 }
+
+export type InputConstructor = <T>(keyMap: _Map<T>, deadzone?: number) => Input<T>;
+
+/**
+ * Gets the device of the provided UserInputType (or the last UserInputType if none is provided)
+ * @param inputType UserInputType
+ * @returns "Desktop" "Touch" or "Controller"
+ */
+export function device(inputType?: Enum.UserInputType): DeviceType;
+
+/**
+ * Updates all input axes in a array
+ *
+ * @param inputs A array of `Input` to update,
+ */
+export function update(inputs: Input<defined>[]): void;
+
+/**
+ * Creates a new input axis with the provided keymap
+ *
+ * @param keyMap It can be a `Map<Enum.KeyCode | string, defined | number>` for deadzone or `Map<Enum.KeyCode, defined>` or Enum.KeyCode[]
+ * @returns The input axis
+ */
+export const input: InputConstructor;
